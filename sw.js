588b0fb03e3e77fb8242ad098582dc4615cf01bd
lagotaquefaltaba <luciavaldatta4@gmail.com>
2026-06-06 13:54:46 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,19 @@
-﻿const CACHE_NAME = 'mi-primer-amor-v1';
+const CACHE_NAME = 'mi-primer-amor-v2';
 const ARCHIVOS_A_CACHEAR = [
-  '/',
-  '/index.html',
-  '/manifest.webmanifest',
-  '/imagenes/logo-principal.png'
+  '/miprimeramor/',
+  '/miprimeramor/index.html',
+  '/miprimeramor/manifest.json',
+  '/miprimeramor/imagenes/logo-principal.png'
 ];
 
-// Instalar
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ARCHIVOS_A_CACHEAR))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ARCHIVOS_A_CACHEAR)));
 });
 
-// Activar
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(nombres => Promise.all(
-      nombres.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))
-    ))
-  );
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(nombres => Promise.all(nombres.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))));
 });
 
-// Buscar en caché primero
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(respuesta => respuesta || fetch(e.request))
-  );
-});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+})
