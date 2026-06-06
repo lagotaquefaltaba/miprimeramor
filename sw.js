245bf4a6e4deffd6328d34a674678776e85c065c
lagotaquefaltaba <luciavaldatta4@gmail.com>
2026-06-06 15:07:44 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,11 @@
-const CACHE_NAME = 'mi-primer-amor-v3'; // Subimos la versión para que el navegador limpie lo anterior
-const ARCHIVOS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './imagenes/logo-principal.jpg'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ARCHIVOS))
-      .then(() => self.skipWaiting())
-  );
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(nombres => 
-      Promise.all(nombres.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
-    ).then(() => self.clients.claim())
-  );
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  if (e.request.url.includes('listen2myradio.com')) {
-    return;
-  }
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener('fetch', (e) => {
+  // Permite que todo pase libremente sin trabarse en caché
 });
