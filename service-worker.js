9caf3dd3a9aa317d051f0b1bbd1fda51311b7a3e
lagotaquefaltaba <luciavaldatta4@gmail.com>
2026-06-06 16:16:42 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+﻿self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Permite que todo pase libremente sin trabarse en caché
+});
