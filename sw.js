19857b5bbe35a8b55758c520468a4234fc959327
lagotaquefaltaba <luciavaldatta4@gmail.com>
2026-06-06 14:51:01 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,32 @@
-const CACHE_NAME = 'mi-primer-amor-v1';
+const CACHE_NAME = 'mi-primer-amor-v3'; // Subimos la versión para que el navegador limpie lo anterior
 const ARCHIVOS = [
-  '/miprimeramor/',
-  '/miprimeramor/index.html',
-  '/miprimeramor/manifest.json',
-  '/miprimeramor/imagenes/logo-principal.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './imagenes/logo-principal.jpg'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ARCHIVOS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ARCHIVOS))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(nombres => 
-    Promise.all(nombres.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
-  ));
+  e.waitUntil(
+    caches.keys().then(nombres => 
+      Promise.all(nombres.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  if (e.request.url.includes('listen2myradio.com')) {
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
